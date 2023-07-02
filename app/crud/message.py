2a32c66fff04aff8c9d0ebf76934aff1775ce069
Anudeep Y <anudeep.yegireddi@gmail.com>
2023-07-02 14:13:28 +0530

    feat(chat): added OpenAI chat functionality

--- app/crud/message.py
+++ app/crud/message.py
@@ -1,8 +1,25 @@
-from crud.crud_base import CRUDBase
-from models.message import Message
-from schemas.message import MessageCreate
+from app.crud.crud_base import CRUDBase
 
-class CRUDMessage(CRUDBase[Message, MessageCreate]):
-    ...
+# from app.models.message import Message
+# from app.schemas.message import MessageCreate
+from app import models
+from app import schemas
+from sqlalchemy.orm import Session
 
-message = CRUDMessage(Message)
+
+class CRUDMessage(CRUDBase[models.Message, schemas.MessageCreate]):
+    def get_messages_by_conv_id(
+        self, db: Session, *, conv_id: int
+    ) -> list[models.Message]:
+        conversation = {"human": [], "agent": []}
+
+        # extract all messages with same conv_id
+        return (
+            db.query(models.Message)
+            .filter(models.Message.conv_id == conv_id)
+            .order_by(models.Message.ts_created)
+            .all()
+        )
+
+
+message = CRUDMessage(models.Message)
